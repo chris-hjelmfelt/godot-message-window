e455ffd75f00aab083ed9d8917ea2f77f5fa43a4
Christine Hjelmfelt <christine.hjelmfelt@gmail.com>
2025-09-26 15:26:37 -0500

    collapsible message window

--- scripts/message_window.gd
+++ scripts/message_window.gd
@@ -1,14 +1,27 @@
 extends Control
 
-@onready var vbox: VBoxContainer = $MarginContainer/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer
+@onready var wide: PanelContainer = $MarginContainer/WidePanel
+@onready var thin: PanelContainer = $MarginContainer/ThinPanel
+@onready var widebox: VBoxContainer = $MarginContainer/WidePanel/MarginContainer/ScrollContainer/VBoxContainer
+@onready var thinlabel: Label = $MarginContainer/ThinPanel/MarginContainer/Label
+@onready var button: TextureButton = $ButtonContainer/TextureButton
 
 
 func _ready() -> void:
+	thin.visible = false
+	button.flip_v = true
 	Globals.display_message.connect(display)
 
 
 func display(message: String):
 	var newline = Label.new()
 	newline.text = message
-	vbox.add_child(newline)
-	vbox.move_child(newline, 0)
+	widebox.add_child(newline)
+	widebox.move_child(newline, 0)
+	thinlabel.text = message  # copy new message to the thin panel
+
+
+func _on_texture_button_pressed() -> void:
+	thin.visible = !thin.visible
+	wide.visible = !wide.visible
+	button.flip_v = !button.flip_v
